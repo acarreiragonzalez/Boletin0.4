0fe85d826eb89d06959ff1fa2be61134b3e34b9a
acarreiragonzalez <acarreiragonzalez@eniac25.danielcastelao.org>
2014-10-09 11:04:47 +0200

    Programacion boletin 0.4 metodos

---
+++ src/boletin04/Boletin04.java
@@ -0,0 +1,20 @@
+
+package boletin04;
+
+
+public class Boletin04 {
+
+   
+    public static void main(String[] args) {
+       //instancio obxecto tipo semaforo
+        Semaforo sema1= new Semaforo();//metodo especial constructor
+       String valor=sema1.dameCor();
+        System.out.println("cor antes de darlle un valor " + valor);
+        sema1.ponCor("verde");
+        valor=sema1.dameCor();
+        System.out.println("cor : "+valor);
+        //outro xeito:
+        System.out.println("cor: " + sema1.dameCor());
+    }
+    
+}
---
+++ src/boletin04/Semaforo.java
@@ -0,0 +1,16 @@
+
+package boletin04;
+
+public class Semaforo {
+    // atributos
+    private String cor;
+    
+    //metodos
+    public void ponCor(String verde){
+        cor=verde;
+    }
+    public String dameCor(){
+        return cor;
+    }
+    
+}
